2c7ae1916c2e5b1ac16286b31fe11ad831b0c493
tremitch504 <trevon@students.alcorn.edu>
2023-01-17 22:53:18 -0600

    added a infinite loop, for if a user enter a invaild answer, ask the same question over again

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ import java.util.Scanner;
 public class Main {
     
     public static void main(String[] args){
-        final byte MONTHS_IN_YEAR = 12;
+        final byte MONTHS_IN_YEAR = 12;  
         final byte PERCENT = 100;
         int principal = 0;
 
